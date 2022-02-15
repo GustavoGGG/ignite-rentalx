c56c871680cbb30f13b300adb05a83e725ee0fa6
Gustavo Gomes <gu.gomes.89@gmail.com>
2022-02-15 00:23:46 -0300

    Criando UsersRepository/Controller/UseCase

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateUserUseCase } from "../CreateUserUseCase";
+
+
+class CreateUserController {
+
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { name, username, password, email, driver_license } = req.body;
+    const createUserCase = container.resolve(CreateUserUseCase)
+
+    await createUserCase.execute({ name, username, password, email, driver_license })
+    return res.status(201).send()
+  }
+
+}
+
+export { CreateUserController }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,6 +3,8 @@ import { ICategoriesRepository } from '../../modules/cars/repositories/protocols
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository'
 import { ISpecificationsRepository } from '../../modules/cars/repositories/protocols/ISpecificationsRepository'
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository'
+import { IUsersRepository } from '../../modules/accounts/repositories/protocols/IUsersRepository'
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository'
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -14,3 +16,7 @@ container.registerSingleton<ISpecificationsRepository>(
   SpecificationsRepository
 )
 
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
+)
