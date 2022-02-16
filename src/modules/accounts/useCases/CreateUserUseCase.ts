ee540fc4b7998c71396a737dad894a3ce884da49
Gustavo Gomes <gu.gomes.89@gmail.com>
2022-02-15 21:09:34 -0300

    Adicionando biblioteca bcrypt e utilizando o hash no password do Users

--- src/modules/accounts/useCases/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../repositories/protocols/IUsersRepository";
+import { hash } from "bcrypt"
 
 
 @injectable()
@@ -11,8 +12,14 @@ class CreateUserUseCase {
     private usersRepository: IUsersRepository
   ) { }
   async execute({ name, password, email, driver_license }: ICreateUserDTO): Promise<void> {
+
+    const userAlreadyExists = await this.usersRepository.findByEmail(email)
+    if (userAlreadyExists) {
+      throw new Error("User already exists")
+    }
+    const passwordHash = await hash(password, 8)
     await this.usersRepository.create({
-      name, password, email, driver_license
+      name, password: passwordHash, email, driver_license
     })
   }
 }
