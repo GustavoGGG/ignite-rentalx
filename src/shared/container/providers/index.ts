0050132f27bf40fa2ee798dcc349b49592897552
Gustavo Gomes <gu.gomes.89@gmail.com>
2022-10-03 23:07:22 -0300

    Create S3StorageProvider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,6 +4,7 @@ import { IDateProvider } from "./DateProvider/protocols/IDateProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 import { IMailProvider } from "./MailProvider/protocols/IMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/protocols/IStorageProvider";
 
 container.registerSingleton<IDateProvider>(
@@ -16,7 +17,12 @@ container.registerInstance<IMailProvider>(
   new EtherealMailProvider()
 )
 
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider
+}
+
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  LocalStorageProvider
+  diskStorage[process.env.DISK]
 )
