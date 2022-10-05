3cd9ede6992f608b0d83fa997b5a1487b62c2617
Gustavo Gomes <gu.gomes.89@gmail.com>
2022-10-04 21:46:46 -0300

    Create router UserProfile

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -5,6 +5,7 @@ import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUse
 
 import uploadConfig from "@config/upload"
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated"
+import { ProfileUserController } from "@modules/accounts/useCases/profileUser/ProfileUserController"
 
 const usersRoutes = Router()
 
@@ -12,9 +13,13 @@ const uploadAvatar = multer(uploadConfig)
 
 const createUserController = new CreateUserController()
 const updateUserAvatarController = new UpdateUserAvatarController()
+const profileUserController = new ProfileUserController()
+
 
 usersRoutes.post("/", createUserController.handle)
 
 usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateUserAvatarController.handle)
+
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle)
 
 export { usersRoutes }
